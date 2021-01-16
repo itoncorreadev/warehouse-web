1f58cf901c9c259a9d829550e4c660d03d83c8a7
Cleiton Correa <cleitoncorreas@gmail.com>
2021-01-15 21:09:16 -0300

    Getting task by id

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -30,8 +30,9 @@ export class TaskService{
     return Promise.resolve(TASKS.slice(0,3));
   }
 
-  public getTask(id: number): Promise<Task>{
-    return this.getTasks()
-      .then(tasks => tasks.find(task => task.id === id));
+  public getTask(id: number): Observable<Task>{
+    let url = `${this.taskUrl}/${id}`;
+    return this.http.get(url)
+      .map((response: Response) => response.json().data as Task);
   }
 }
