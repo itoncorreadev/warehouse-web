a74b118ebdbc0ccdce595c5a311d88d59bf9aeee
Cleiton Correa <cleitoncorreas@gmail.com>
2021-01-18 18:26:20 -0300

    Adding SignInFormComponent base class

---
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormUtils } from "../shared/form.utils";
+
+@Component({
+  selector: 'sign-in-form',
+  templateUrl: 'sign-in-form.component.html'
+})
+
+export class SignInFormComponent{
+  public form: FormGroup;
+  public formUtils: FormUtils;
+
+  public constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
+
+    this.formUtils = new FormUtils(this.form);
+  }
+
+  public signInUser(){
+    console.log("Formulário de SignIn Enviado!");
+    console.log("this.form.value");
+  }
+}
